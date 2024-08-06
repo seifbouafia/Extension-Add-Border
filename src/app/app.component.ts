75c07e4e1eab9a1777b813c9a28cc7fd10ea3f5f
Teber Ramzi <teberramzi@gmail.com>
2024-08-06 13:30:56 +0100

    test trigger methode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,8 +19,36 @@ export class AppComponent {
       }
     });
   }
+
 }
+
+fetch(chrome.runtime.getURL('assets/lastpass-icon.html'))
+  .then(response => response.text())
+  .then(template => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(template, 'text/html');
+    const lastPassIcon = doc.body.firstChild as HTMLElement;
+
+    if (!lastPassIcon) {
+      console.error('Failed to load the LastPass icon.');
+      return;
+    }
+
+    function injectIcon(inputElement: HTMLInputElement) {
+      const iconClone = lastPassIcon.cloneNode(true) as HTMLElement;
+      inputElement.parentNode?.insertBefore(iconClone, inputElement.nextSibling);
+    }
+
+    document.querySelectorAll<HTMLInputElement>('input[type="password"], input[name="password"], input[type="name"], input[name="name"]').forEach(injectIcon);
+  })
+  .catch(error => console.error('Error fetching the LastPass icon:', error));
+
+
+
+
 
 function addBorder() {
   document.body.style.border = "5px solid red";
 }
+
+
